6ac61cb1e4a51550689d054e952d12293ddd3b1d
Damien Nelson <dhnisaman@gmail.com>
2023-05-21 14:43:25 -0400

    commit tree f885280fe116279ae470e17ea12f11b51e009b89

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+    
+    def render_not_found(exeption)
+        render json: {error: exeption.message}, status: :not_found
+    end
 end
